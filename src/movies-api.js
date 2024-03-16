e1e9ce3931e4de6b2a42e79ee3e46b14e6a70b7d
TetiZ <tommytaba99@gmail.com>
2024-03-16 15:17:33 +0100

    initial project

---
+++ src/movies-api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org";
+
+// const options = {
+//     headers: {
+//         Authorization:
+//     }
+// }
+
+// export const getMovies = async () => {
+//     const response = await.axios.get('');
+// };
